a94f102869fea69fb834181bdbac97088324cfff
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 22:27:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 def prime?(n) 
-  puts (2..n/2.floor).to_a
   if n < 2 || (2..n/2.floor).to_a.any? {|divisor| n % divisor == 0}
     return false
   else 
     return true 
   end
 end 
-
-puts prime?(105557)
